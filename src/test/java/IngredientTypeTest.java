89bf0621e7c6c2a764a6670153cc09d082169a1d
Stas Basov <stasbasov@yandex.ru>
2024-05-21 14:07:04 +0300

    Добавил тесты

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class IngredientTypeTest {
+import org.junit.Test;
+import praktikum.IngredientType;
+import static org.junit.Assert.*;
+public class IngredientTypeTest {
+    @Test
+    public void fillingNotNull() {
+        assertNotNull("Тест провален:ты забыл указать начинку =(", IngredientType.valueOf("FILLING"));
+    }
+    @Test
+    public void sauceNotNull() {
+        assertNotNull("Тест провален:ты забыл указать соус =(", IngredientType.valueOf("SAUCE"));
+    }
 }
